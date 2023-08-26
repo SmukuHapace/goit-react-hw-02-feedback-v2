c3bb42f28d1e1b93dfb0dfefe334cdcd048fb490
Mateusz Kubiak <smuku.mk@gmail.com>
2023-08-26 14:21:22 +0200

    prop-types-add

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
 export const Statistics = ({
@@ -16,4 +17,12 @@ export const Statistics = ({
       <li>Positive feedback: {positivePercentage}%</li>
     </ul>
   );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
 };
